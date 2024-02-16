90f959e21af9a25b2788d86e39832c8dc3552479
Zvki1 <rzaki@hotmail.fr>
2024-02-16 22:37:54 +0100

    finishing the hero section

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,23 +1,25 @@
 import "../Hero.css";
 import Motif from "../assets/Motif.svg"
+import Motif2 from "../assets/lg.svg"
 import MemberCard from "./MemberCard";
 
 const Hero = () => {
   return (
-    <div className=" pt-24 bg-[#0A090F] text-white relative pb-24">
+    <div className=" pt-24 bg-[#0A090F] text-white relative pb-20 lg:mx-32 lg:rounded-b-[40px]">
       {/* h1 and cta div */}
-      <div className="pl-7 flex flex-col gap-10 items-start">
-      <h1 className="text-[36px] font-bold  ">
+      <div className="pl-7 lg:pl-16 flex flex-col gap-10 items-start">
+      <h1 className="text-[36px] lg:text-[56px] font-bold  ">
         Empower Your <br />
         Startup Journey <br /> with 
       <span className="gradient-text"> Career-Up</span> 
       </h1>
-      <button type="button" className="text-white bg-gradient-to-r from-[#4269E1]  to-[#BEE3F8] hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Try it now</button>
+      <button type="button" className="text-white bg-gradient-to-r from-[#4269E1]  to-[#BEE3F8] hover:bg-gradient-to-br lg:text-2xl focus:ring-4 focus:outline-none focus:ring-blue-300  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Try it now</button>
       </div>
       {/* image */}
-      <img width={90} src={Motif} alt=""  className="absolute z-0 top-8 right-0"/>
+      <img width={90} src={Motif} alt=""  className="absolute z-0 top-8 right-0 lg:hidden"/>
+      <img width={290} src={Motif2} alt=""  className="hidden lg:block lg:absolute z-0 top-8 right-0 "/>
       {/* the two cards */}
-      <div className=" pt-20 w-full flex flex-col items-center gap-7"> 
+      <div className=" pt-20 w-full flex flex-col lg:flex-row  items-center gap-7 lg:pl-16 lg:relative"> 
         <MemberCard role="Designer" name="Raouf benbellat"/>
         <MemberCard role="Marketing" name="Raouf benbellat"/>
       </div>
--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -2,7 +2,8 @@ import Logo from '../assets/logo.png'
 import Hamburger from '../assets/hamburger.png'
 const NavBar = () => {
   return (
-   <nav className=' flex flex-row justify-between pt-6 px-7  w-full z-10 absolute '>
+    <div>
+   <nav className=' flex flex-row lg:hidden justify-between pt-6 px-7  w-full z-10 absolute '>
     <a href="#">
         <img width="34px" src={Logo} alt="lg" />
     </a>
@@ -10,6 +11,20 @@ const NavBar = () => {
         <img width="36px" src={Hamburger} alt="hm" />
     </button>
    </nav>
+   <nav className='hidden lg:flex flex-row justify-between items-center bg-[#0A090F] text-white font-meduim text-[20px] py-5 mx-32 px-5'>
+   <a href="#">
+        <img width="34px" src={Logo} alt="lg" />
+    </a>
+    <ul className='flex flex-row gap-5'>
+      <li className='hover:text-[#BEE3F8]'> <a href=""> Home </a></li>
+      <li className='hover:text-[#BEE3F8]'> <a href=""> About Us </a></li>
+      <li className='hover:text-[#BEE3F8]'> <a href=""> Our Flow </a></li>
+      <li className='hover:text-[#BEE3F8]'> <a href=""> FAQ </a></li>
+    </ul>
+    <button type="button" className="text-white bg-gradient-to-r from-[#4269E1]  to-[#BEE3F8] hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300  font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Build your team </button>
+   </nav>
+   </div>
+   
   )
 }
 
