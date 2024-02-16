8dc2c4ab686b459de079ba5d8740c75dda429c08
Zvki1 <rzaki@hotmail.fr>
2024-02-17 00:21:11 +0100

    adding verified team tiltle

--- src/component/Teams.jsx
+++ src/component/Teams.jsx
@@ -2,10 +2,13 @@
 
 const Teams = () => {
   return (
+    <>
     <div className="overflow-hidden overflow-ellipsis whitespace-nowrap flex items-center justify-center ">
     <h1 className="text-[80px] text-black opacity-10 -rotate-6 font-bold text-crop py-6 lg:hidden ">VERIFIED TEAMS</h1>
     <h1 className="text-[120px] text-black opacity-10 -rotate-3 font-bold text-crop py-6 hidden lg:block  ">VERIFIED TEAMS VERIFIED TEAMS</h1>
     </div>
+    <h2 className="text-center text-[25px] opacity-20 font-bold md:hidden">Verified Teams  </h2>
+    </>
   )
 }
 
